2a8132fb0e016ede198d3c1ec6a7a65de39676e7
Anders <andersmyrmel@gmail.com>
2022-09-01 18:34:41 +0200

    Fixed assignment 2_2

--- Laboppgave2/1/Script.js
+++ Laboppgave2/1/Script.js
@@ -6,6 +6,7 @@ let centerX = c.width / 2; // x center av sirkelen
 let centerY = c.height / 2; // y center av sirkelen
 let size = 550; // størrelse på sirkel
 let r = size / 2; // radius
+let points = []; // Array of all points on circle
 
 function calculateCircle(v) {
 	const startx = r * Math.cos(0 * (Math.PI / (v / 2))) + centerX;
@@ -17,20 +18,44 @@ function calculateCircle(v) {
 		let angle = i * (Math.PI / (v / 2));
 		let x = r * Math.cos(angle) + centerX;
 		let y = r * Math.sin(angle) + centerY;
-		draw(x, y);
+		drawCircle(x, y);
+		points.push({ x, y });
 	}
-	draw(startx, starty);
+	drawCircle(startx, starty);
 }
 
-function draw(x, y) {
+function drawArt(kx) {
 	ctx.strokeStyle = 'black';
-	ctx.lineWidth = 3;
+	ctx.lineWidth = 1;
+	for (let index = 0; index < points.length; index++) {
+		if (index == 0) {
+			drawLine(points, index, 1);
+		} else if (points[index * kx] !== undefined) {
+			drawLine(points, index, index * kx);
+		} else if (points[index * kx] == undefined) {
+			drawLine(points, index, (index * kx) % points.length);
+		}
+	}
+}
+
+function drawLine(array, from, to) {
+	ctx.beginPath();
+	ctx.moveTo(array[from].x, array[from].y);
+	ctx.lineTo(array[to].x, array[to].y);
+	ctx.stroke();
+	ctx.closePath();
+}
+
+function drawCircle(x, y) {
+	ctx.strokeStyle = 'black';
+	ctx.lineWidth = 2;
 	ctx.lineTo(x, y);
 	ctx.stroke();
 }
 
 source.addEventListener('input', function (event) {
 	ctx.clearRect(0, 0, 600, 600); // clear previous canvas at start of every update
+	points = [];
 	let result = event.target.value;
 	calculateCircle(result);
 });
