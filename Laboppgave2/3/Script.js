fe0b5392e65b1087d2e76ad8c633594e09d99191
Anders <andersmyrmel@gmail.com>
2022-09-02 18:06:33 +0200

    Finished assignment 2_3

--- Laboppgave2/3/Script.js
+++ Laboppgave2/3/Script.js
@@ -1,57 +1,90 @@
 const c = document.getElementById('myCanvas');
 const ctx = c.getContext('2d');
 
-// x = (1-p) * x0 + P*x1
-// y = (1-p) * y0 + P*y1
-// p ∈ [0,1]
+let points = {
+	x0: 0,
+	y0: 0,
+	x1: 100,
+	y1: 0,
+	x2: 100,
+	y2: 100,
+	x3: 0,
+	y3: 100,
+};
+let backwards = true;
+let p = 0.1;
+let previousX = 0;
+let previousY = 0;
+let numberOfSquaresWithin = 20;
 
-let x0 = 300;
-let y0 = 150;
-let x1 = 300;
-let y1 = 450;
+for (let rows = 0; rows < 6; rows++) {
+	for (let cols = 0; cols < 6; cols++) {
+		points = updateRowCols(cols, rows, points);
+		drawSquare(
+			[points.x0, points.y0],
+			[points.x1, points.y1],
+			[points.x2, points.y2],
+			[points.x3, points.y3]
+		);
 
-for (let i = 0.1; i < 1; i += 0.1) {
-	let x = (1 - i) * x0 + i * x1;
-	let y = (1 - i) * y0 + i * y1;
-	drawRect(x1, y1, x, y);
-	console.log(x, y);
-	x1 = x;
-	y1 = y;
+		if (!backwards) {
+			p = 0.9;
+			backwards = true;
+		} else {
+			p = 0.1;
+			backwards = false;
+		}
+
+		for (let i = 0; i <= numberOfSquaresWithin; i++) {
+			points = getNewSquare(points);
+			drawSquare(
+				[points.x0, points.y0],
+				[points.x1, points.y1],
+				[points.x2, points.y2],
+				[points.x3, points.y3]
+			);
+		}
+	}
 }
 
-//let x = (10 - 1) * x0 + 1 * x1;
-//let y = (10 - 1) * y0 + 1 * y1;
-//console.log(x, y);
+// Calcuate row and col position for the 36 squares
+function updateRowCols(cols, rows, points) {
+	points.x0 = cols * 100;
+	points.y0 = rows * 100;
+	points.x1 = 100 + cols * 100;
+	points.y1 = rows * 100;
+	points.x2 = 100 + cols * 100;
+	points.y2 = 100 + rows * 100;
+	points.x3 = cols * 100;
+	points.y3 = 100 + rows * 100;
+	return points;
+}
 
-//drawRect(x0, y0, x1, y1);
+// Caclulate coordinates for new square within previous square
+// x = (1 - p) * x0 + p * x1, p ∈ [0,1]
+function getNewSquare(points) {
+	previousX = points.x0;
+	previousY = points.y0;
+	points.x0 = (1 - p) * points.x0 + p * points.x1;
+	points.y0 = (1 - p) * points.y0 + p * points.y1;
+	points.x1 = (1 - p) * points.x1 + p * points.x2;
+	points.y1 = (1 - p) * points.y1 + p * points.y2;
+	points.x2 = (1 - p) * points.x2 + p * points.x3;
+	points.y2 = (1 - p) * points.y2 + p * points.y3;
+	points.x3 = (1 - p) * points.x3 + p * previousX;
+	points.y3 = (1 - p) * points.y3 + p * previousY;
+	return points;
+}
 
-function drawRect(x0, y0, x1, y1) {
+// Draw square between four x,y coordinates
+function drawSquare(corner1, corner2, corner3, corner4) {
 	ctx.beginPath();
-	ctx.moveTo(x0, y0);
-	ctx.lineTo(y0, x1);
-	ctx.lineTo(x0, y1);
-	ctx.lineTo(y1, x0);
+	ctx.moveTo(corner1[0], corner1[1]);
+	ctx.lineTo(corner2[0], corner2[1]);
+	ctx.lineTo(corner3[0], corner3[1]);
+	ctx.lineTo(corner4[0], corner4[1]);
 	ctx.closePath();
 	ctx.stroke();
-}
-
-c.onmousemove = function (e) {
-	var pos = getMousePos(this, e), /// provide this canvas and event
-		x = pos.x,
-		y = pos.y;
-	console.log(pos);
-
-	/// check x and y against the grid
-};
-
-function getMousePos(canvas, e) {
-	/// getBoundingClientRect is supported in most browsers and gives you
-	/// the absolute geometry of an element
-	var rect = canvas.getBoundingClientRect();
-
-	/// as mouse event coords are relative to document you need to
-	/// subtract the element's left and top position:
-	return { x: e.clientX - rect.left, y: e.clientY - rect.top };
 }
 
 // change page on button click
