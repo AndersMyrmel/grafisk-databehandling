8c3a5c072b742003f78a28fdf4ff4faf74ccedce
Anders <andersmyrmel@gmail.com>
2022-11-01 20:31:10 +0100

    added pyramid

--- Laboppgave9/Script.js
+++ Laboppgave9/Script.js
@@ -9,34 +9,34 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-//const geometry = new THREE.BoxGeometry(1, 1, 1);
-//const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-//const cube = new THREE.Mesh(geometry, material);
-//scene.add(cube);
-//
-//camera.position.z = 5;
-
 const geometry = new THREE.BufferGeometry();
 // create a simple square shape. We duplicate the top left and bottom right
 // vertices because each vertex needs to appear once per triangle.
 const vertices = new Float32Array([
-	-1.0, -1.0, 1.0, 1.0, -1.0, 1.0, 1.0, 1.0, 1.0,
+	-1, 1, 1, 0, -1, 0, 1, 1, 1,
 
-	1.0, 1.0, 1.0, -1.0, 1.0, 1.0, -1.0, -1.0, 1.0,
+	-1, 1, -1, 0, -1, 0, -1, 1, 1,
+
+	1, 1, 1, 0, -1, 0, 1, 1, -1,
+
+	1, 1, -1, 0, -1, 0, -1, 1, -1,
+
+	-1, 1, -1, -1, 1, 1, 1, 1, -1,
+
+	1, 1, -1, -1, 1, 1, 1, 1, 1,
 ]);
 
 // itemSize = 3 because there are 3 values (components) per vertex
 geometry.setAttribute('position', new THREE.BufferAttribute(vertices, 3));
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
 const mesh = new THREE.Mesh(geometry, material);
-
 scene.add(mesh);
 camera.position.z = 5;
 
 function animate() {
 	requestAnimationFrame(animate);
-	//mesh.rotation.x += 0.01;
-	//mesh.rotation.y += 0.01;
+	mesh.rotation.x += 0.004;
+	mesh.rotation.y += 0.004;
 	renderer.render(scene, camera);
 }
 
