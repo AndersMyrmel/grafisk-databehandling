c630051a505bc2f90c3d579e30a33496ae40476b
Anders <andersmyrmel@gmail.com>
2022-11-01 21:03:30 +0100

    add light

--- Laboppgave9/Script.js
+++ Laboppgave9/Script.js
@@ -11,8 +11,6 @@ document.body.appendChild(renderer.domElement);
 var controls = new THREE.OrbitControls(camera, renderer.domElement);
 
 const geometry = new THREE.BufferGeometry();
-// create a simple square shape. We duplicate the top left and bottom right
-// vertices because each vertex needs to appear once per triangle.
 const vertices = new Float32Array([
 	-1, 1, 1, 0, -1, 0, 1, 1, 1,
 
@@ -26,8 +24,6 @@ const vertices = new Float32Array([
 
 	1, 1, -1, -1, 1, 1, 1, 1, 1,
 ]);
-
-// itemSize = 3 because there are 3 values (components) per vertex
 geometry.setAttribute('position', new THREE.BufferAttribute(vertices, 3));
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
 const mesh = new THREE.Mesh(geometry, material);
