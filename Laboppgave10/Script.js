b44a5f6bcf137047976c4a420b94f0234c139b9a
Anders <andersmyrmel@gmail.com>
2022-11-09 02:15:01 +0100

    add grid

--- Laboppgave10/Script.js
+++ Laboppgave10/Script.js
@@ -13,17 +13,51 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 var controls = new THREE.OrbitControls(camera, renderer.domElement);
 
+const grid = new THREE.GridHelper(2000, 20, '#000000', '#9a9a9a');
+grid.material.opacity = 0.2;
+grid.material.transparent = true;
+scene.add(grid);
+
 const light = new THREE.AmbientLight(0x404040);
 scene.add(light);
 
-let loadedModel;
-const gltfLoader = new THREE.GLTFLoader();
-gltfLoader.load('./assets/WindTurbine.glb', (gltfScene) => {
-	gltfScene.scene.position.y = -10;
-	gltfScene.scene.position.z = -10;
-	gltfScene.scene.scale.set(1, 1, 1);
-	scene.add(gltfScene.scene);
-});
+const GLTObjectModel = {
+	Object_01: null,
+	Object_02: null,
+	Object_03: null,
+};
+
+const loader = new THREE.GLTFLoader();
+loader.load('./assets/WindTurbine.glb', GLTFLoadDone);
+
+function GLTFLoadDone(GLTFStructure) {
+	GLTFStructure.scene.traverse(GLTFEachObject);
+	scene.add(GLTFStructure.scene);
+}
+
+function GLTFEachObject(aObject) {
+	if (aObject.isMesh) {
+		aObject.castShadow = true;
+
+		aObject.receiveShadow = true;
+	} else if (aObject.isGroup) {
+		aObject.castShadow = true;
+
+		GLTObjectModel[aObject.name] = aObject;
+	}
+}
+
+//let loadedModel;
+//const gltfLoader = new THREE.GLTFLoader();
+//gltfLoader.load('./assets/WindTurbine.glb', (gltfScene) => {
+//	loadedModel = gltfScene;
+//	gltfScene.scene.position.y = -10;
+//	gltfScene.scene.position.z = -10;
+//	gltfScene.scene.scale.set(1, 1, 1);
+//	scene.add(gltfScene.scene);
+//});
+
+console.log(GLTObjectModel);
 
 function animate() {
 	requestAnimationFrame(animate);
